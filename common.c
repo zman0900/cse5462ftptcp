ee618f58f662b3769f65b573aa6f175e05b7b2a4
Dan Ziemba <zman0900@gmail.com>
2013-01-23 01:33:35 -0500

    Really fix tabs

--- common.c
+++ common.c
@@ -3,11 +3,11 @@
 #include "common.h"
 
 void getInAddrString(int af, struct sockaddr *sa, char *dst, socklen_t size) {
-    const void *src;
-    if (sa->sa_family == AF_INET) {
-        src = &(((struct sockaddr_in*)sa)->sin_addr);
-    } else {
-        src = &(((struct sockaddr_in6*)sa)->sin6_addr);
-    }
-    inet_ntop(af, src, dst, size);
+	const void *src;
+	if (sa->sa_family == AF_INET) {
+		src = &(((struct sockaddr_in*)sa)->sin_addr);
+	} else {
+		src = &(((struct sockaddr_in6*)sa)->sin6_addr);
+	}
+	inet_ntop(af, src, dst, size);
 }
