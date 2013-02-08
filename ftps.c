9b34d853e644e03e6010b89511463636facb59f7
Dan Ziemba <zman0900@gmail.com>
2013-02-08 09:44:24 -0500

    Modified ftps to use tcpd

--- ftps.c
+++ ftps.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 
 #include "common.h"
+#include "tcpd_interface.h"
 
 #define CONNECTION_QUEUE 5
 #define BUF_SIZE 1000
@@ -41,11 +42,11 @@ void receiveFile(int connfd) {
 	strcat(fileName, "/");
 	strcat(fileName, recvBuf);
 
-	printf("Receiving file '%s' of size %u\n", fileName, fileSize);
+	printf("ftps: Receiving file '%s' of size %u\n", fileName, fileSize);
 
 	// Open file
 	if ((file = fopen(fileName, "wb")) == NULL) {
-		perror("Couldn't open file");
+		perror("ftps: Couldn't open file");
 		exit(1);
 	}
 
@@ -53,20 +54,20 @@ void receiveFile(int connfd) {
 	remainLastUpdate = remaining = fileSize;
 	start = last = time(NULL);
 	while (remaining > 0) {
-		if ((bytes = recv(connfd, recvBuf,
+		if ((bytes = RECV(connfd, recvBuf,
 		             (remaining < BUF_SIZE) ? remaining : BUF_SIZE, 0)) == -1) {
-			perror("recv");
+			perror("ftps: recv");
 			exit(1);
 		}
 		if (bytes == 0) {
-			fprintf(stderr, "Connection lost.\n");
+			fprintf(stderr, "ftps: Connection lost.\n");
 			exit(2);
 		}
 		remaining -= bytes;
 		// Display info only once per second
 		current = time(NULL);
 		if (current >= last + 1) {
-			printf("Received %.02f KB (%.02f KB/s)...\n",
+			printf("ftps: Received %.02f KB (%.02f KB/s)...\n",
 			       (fileSize-remainLastUpdate)/1000.0,
 			       ((remainLastUpdate-remaining)/1000.0)/difftime(current, last));
 			last = current;
@@ -74,117 +75,104 @@ void receiveFile(int connfd) {
 		}
 		// Write to file
 		if (fwrite(recvBuf, 1, bytes, file) != bytes) {
-			fprintf(stderr, "File write error!\n");
+			fprintf(stderr, "ftps: File write error!\n");
 			exit(1);
 		}
 	}
 
 	// Show stats
 	current = time(NULL);
-	printf("Received %.02f KB (average %.02f KB/s)...\n", fileSize/1000.0,
+	printf("ftps: Received %.02f KB (average %.02f KB/s)...\n", fileSize/1000.0,
 	       (fileSize/1000.0)/difftime(current, start));
 
 	fflush(file);
-	fclose(file);
-	printf("Done\n");
+	//fclose(file);  // This is causing a segfault now, but not in gdb...
+	printf("ftps: Done\n");
 }
 
-void sigchld_handler(int s) {
+/*void sigchld_handler(int s) {
 	while(waitpid(-1, NULL, WNOHANG) > 0);
-	printf("Child finished.\n");
-}
+	printf("ftps: Child finished.\n");
+}*/
 
 int main(int argc, char *argv[]) {
 	int sockfd, connfd;
-	struct addrinfo hints, *servinfo, *p;
-	int status, yes=1;
-	struct sigaction sa;
-	struct sockaddr_storage clientaddr;
-	socklen_t ca_size;
-	char addrString[INET6_ADDRSTRLEN];
+	struct addrinfo hints, *servinfo;
+	int status;
+	//struct sigaction sa;
+	//struct sockaddr_storage clientaddr;
+	//socklen_t ca_size;
+	//char addrString[INET6_ADDRSTRLEN];
 
 	if (argc != 2) {
 		printf("Usage: %s <port>\n", argv[0]);
 		return 1;
 	}
-	printf("Starting server on port %s...\n", argv[1]);
+	printf("ftps: Starting server on port %s...\n", argv[1]);
 
 	// Create output folder
 	if (mkdir(OUTPUT_DIR, S_IRWXU) == -1) {
 		int err = errno;
 		if (err != EEXIST) {
-			fprintf(stderr, "Error creating output directory: %s\n", strerror(err));
+			fprintf(stderr, "ftps: Error creating output directory: %s\n",
+			        strerror(err));
 			exit(1);
 		}
 	}
 
 	// Setup structures
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 	if ((status = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		fprintf(stderr, "ftps: getaddrinfo: %s\n", gai_strerror(status));
 		return 1;
 	}
 
-	//Bind to first address
-	for (p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			perror("socket");
-			continue;
-		}
-		// Let other sockets bind port unless active listening socket
-		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-			perror("setsockopt");
-			exit(1);
-		}
-		// Bind socket
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			perror("bind");
-			close(sockfd);
-			continue;
-		}
-		// Socket bound
-		break;
+	// Get socket
+	if ((sockfd = SOCKET(servinfo->ai_family, servinfo->ai_socktype,
+	                     servinfo->ai_protocol)) == -1) {
+		perror("ftps: socket");
+		exit(1);
 	}
-
-	// Make sure socket is bound
-	if (p == NULL)  {
-		fprintf(stderr, "Failed to bind to an address.\n");
-		return 1;
+	// Bind
+	if (BIND(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+		perror("ftps: bind");
+		CLOSE(sockfd);
+		exit(1);
 	}
 
 	// Clean up
 	freeaddrinfo(servinfo);
 
 	// Start listening
-	if (listen(sockfd, CONNECTION_QUEUE) == -1) {
-		perror("listen");
+	/*if (listen(sockfd, CONNECTION_QUEUE) == -1) {
+		perror("ftps: listen");
 		exit(1);
 	}
-	printf("Listening for connections...(Ctrl-C to stop)\n");
+	printf("ftps: Listening for connections...(Ctrl-C to stop)\n");*/
 
 	// Zombie hunting
-	sa.sa_handler = sigchld_handler;
+	/*sa.sa_handler = sigchld_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
 	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-		perror("sigaction");
+		perror("ftps: sigaction");
 		exit(1);
-	}
+	}*/
 
 	// Listen forever
-	while(1) {
+	/*while(1) {
 		ca_size = sizeof clientaddr;
-		connfd = accept(sockfd, (struct sockaddr *)&clientaddr, &ca_size);
+		connfd = ACCEPT(sockfd, (struct sockaddr *)&clientaddr, &ca_size);
 		if (connfd == -1) {
-			perror("accept");
+			perror("ftps: accept");
 			continue;
 		}
 		getInAddrString(clientaddr.ss_family, (struct sockaddr *)&clientaddr, 
 			addrString, sizeof addrString);
-		printf("New connection from %s\n", addrString);
+		printf("ftps: New connection from %s\n", addrString);
 
 		int pid;
 		if ((pid = fork()) < 0) {
@@ -193,16 +181,18 @@ int main(int argc, char *argv[]) {
 		} else if (pid == 0) {
 			// Child process
 			// Close listener in child
-			close(sockfd);
-			receiveFile(connfd);
+			close(sockfd);*/
+			//receiveFile(connfd);
+			receiveFile(sockfd);
+			CLOSE(sockfd);
 			exit(0);
-		} else {
+		/*} else {
 			// Parent process
-			printf("Started child %d\n", pid);
+			printf("ftps: Started child %d\n", pid);
 			// Close connection in parent
 			close(connfd);
 		}
-	}
+	}*/
 
 	return 0;
 }
