de9ff4193b5e5d4da5b9237e3f045419a732a400
Dan Ziemba <zman0900@gmail.com>
2013-02-08 07:52:34 -0500

    Ftpc modified to work with tcpd

--- ftpc.c
+++ ftpc.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 
 #include "common.h"
+#include "tcpd_interface.h"
 
 #define BUF_SIZE 1000
 
@@ -23,44 +24,41 @@ int fileNameLen;
 uint32_t fileSize;
 
 void openConnection(char *host, char *port) {
-	struct addrinfo hints, *servinfo, *p;
+	struct addrinfo hints, *servinfo;
 	int status;
 	char addrString[INET6_ADDRSTRLEN];
 
 	// Setup structures
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	if ((status = getaddrinfo(host, port, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+		fprintf(stderr, "ftpc: getaddrinfo: %s\n", gai_strerror(status));
 		exit(1);
 	}
 
-	// Connect to first result
-	for (p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-			perror("socket");
-			continue;
-		}
-		// Try connect
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			perror("connect");
-			close(sockfd);
-			continue;
-		}
-		// Socket connected
-		break;
+	// Get socket
+	if ((sockfd = SOCKET(servinfo->ai_family, servinfo->ai_socktype,
+	                     servinfo->ai_protocol)) == -1) {
+		perror("ftpc: socket");
+		exit(1);
 	}
-
-	// Make sure socket is connected
-	if (p == NULL)  {
-		fprintf(stderr, "Failed to connect.\n");
+	// Bind
+	if (BIND(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+		perror("ftpc: bind");
+		CLOSE(sockfd);
+		exit(1);
+	}
+	// Connect
+	if (CONNECT(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+		perror("ftpc: connect");
+		CLOSE(sockfd);
 		exit(1);
 	}
 
-	getInAddrString(p->ai_family, (struct sockaddr *)p->ai_addr, 
+	getInAddrString(servinfo->ai_family, (struct sockaddr *)servinfo->ai_addr, 
 			addrString, sizeof addrString);
-	printf("Connected to %s\n", addrString);
+	printf("ftpc: Connected to %s\n", addrString);
 
 	// Clean up
 	freeaddrinfo(servinfo);
@@ -70,22 +68,22 @@ void openFile() {
 	struct stat st;
 
 	if ((fileNameLen = strlen(fileName)) > FNAME_LEN - 1) {
-		fprintf(stderr, "File name must be less than %d characters!\n", 
+		fprintf(stderr, "ftpc: File name must be less than %d characters!\n", 
 		        FNAME_LEN);
 		exit(1);
 	}
 	if ((file = fopen(filePath, "rb")) == NULL) {
-		perror("Couldn't open file");
+		perror("ftpc: Couldn't open file");
 		exit(1);
 	}
 	// Get size
 	if (fstat(fileno(file), &st) == -1) {
-		perror("Can't determine size of file");
+		perror("ftpc: Can't determine size of file");
 		exit(1);
 	}
 	// Make sure size fits in 32bit
 	if (st.st_size > UINT32_MAX) {
-		fprintf(stderr, "File too large!\n");
+		fprintf(stderr, "ftpc: File too large!\n");
 		exit(1);
 	}
 	fileSize = st.st_size;
@@ -101,29 +99,42 @@ void sendFile() {
 	// Send size
 	memcpy(sendBuf, &netSize, 4);
 	len = 4;
-	if (sendAll(sockfd, sendBuf, &len) != 0) exit(1);
+	if (SEND(sockfd, sendBuf, len, 0) <= 0) {
+		fprintf(stderr, "ftpc: Send error!\n");
+		CLOSE(sockfd);
+		exit(1);
+	}
 
 	// Send name in 20 bytes
 	memset(sendBuf, '\0', FNAME_LEN);  // Not really necessary...
 	memcpy(sendBuf, fileName, fileNameLen + 1);
 	len = FNAME_LEN;
-	if (sendAll(sockfd, sendBuf, &len) != 0) exit(1);
+	if (SEND(sockfd, sendBuf, len, 0) <= 0) {
+		fprintf(stderr, "ftpc: Send error!\n");
+		CLOSE(sockfd);
+		exit(1);
+	}
 
 	// Read and send file
 	remainLastUpdate = remaining = fileSize;
 	start = last = time(NULL);
 	while (remaining >= BUF_SIZE) {
 		if (fread(sendBuf, 1, BUF_SIZE, file) != BUF_SIZE) {
-			fprintf(stderr, "File read error!\n");
+			fprintf(stderr, "ftpc: File read error!\n");
+			CLOSE(sockfd);
 			exit(1);
 		}
 		len = BUF_SIZE;
-		if (sendAll(sockfd, sendBuf, &len) != 0) exit(1);
+		if (SEND(sockfd, sendBuf, len, 0) <= 0) {
+			fprintf(stderr, "ftpc: Send error!\n");
+			CLOSE(sockfd);
+			exit(1);
+		}
 		remaining -= BUF_SIZE;
 		// Display info only once per second
 		current = time(NULL);
 		if (current >= last + 1) {
-			printf("Sent %.02f KB (%.02f KB/s)...\n",
+			printf("ftpc: Sent %.02f KB (%.02f KB/s)...\n",
 			       (fileSize-remainLastUpdate)/1000.0,
 			       ((remainLastUpdate-remaining)/1000.0)/difftime(current, last));
 			last = current;
@@ -132,18 +143,22 @@ void sendFile() {
 	}
 	// Send any remaining less buffer size
 	if (fread(sendBuf, 1, remaining, file) != remaining) {
-		fprintf(stderr, "File read error!\n");
+		fprintf(stderr, "ftpc: File read error!\n");
 	}
 	len = remaining;
-	if (sendAll(sockfd, sendBuf, &len) != 0) exit(1);
+	if (SEND(sockfd, sendBuf, len, 0) <= 0) {
+		fprintf(stderr, "ftpc: Send error!\n");
+		CLOSE(sockfd);
+		exit(1);
+	}
 
 	// Show stats
 	current = time(NULL);
-	printf("Sent %.02f KB (average %.02f KB/s)...\n", fileSize/1000.0,
+	printf("ftpc: Sent %.02f KB (average %.02f KB/s)...\n", fileSize/1000.0,
 	       (fileSize/1000.0)/difftime(current, start));
 
 	fclose(file);
-	printf("Done\n");
+	printf("ftpc: Done\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -151,15 +166,19 @@ int main(int argc, char *argv[]) {
 		printf("Usage: %s <host> <port> <file>\n", argv[0]);
 		return 1;
 	}
-	printf("Starting client...\n");
+	printf("ftpc: Starting client...\n");
 
 	fileName = basename(argv[3]);
 	filePath = argv[3];
 	openFile();
-	printf("Sending file %s of size %u bytes.\n", fileName, fileSize);
+	printf("ftpc: Sending file %s of size %u bytes.\n", fileName, fileSize);
 
 	openConnection(argv[1], argv[2]);
 	sendFile();
+
+	// Close connection
+	sleep(5); //make sure everything is done
+	CLOSE(sockfd);
 
 	return 0;
 }
