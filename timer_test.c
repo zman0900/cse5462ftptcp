5c8f36fc9db3442f5373a29b0d90015965e10b42
Dan Ziemba <zman0900@gmail.com>
2013-03-04 07:10:18 -0500

    More output for timer-test

--- timer_test.c
+++ timer_test.c
@@ -27,30 +27,40 @@ int main(int argc, char *argv[]) {
 	timer->tv_usec = 0;
 	if (timer_start(timer_socket, timer, 1) < 0) {
 		fprintf(stderr, "timer-test: Failed to start timer 1\n");
+	} else {
+		printf("Started timer 1\n");
 	}
 
 	timer->tv_sec = 10;
 	timer->tv_usec = 0;
 	if (timer_start(timer_socket, timer, 2) < 0) {
 		fprintf(stderr, "timer-test: Failed to start timer 2\n");
+	} else {
+		printf("Started timer 2\n");
 	}
 
 	timer->tv_sec = 30;
 	timer->tv_usec = 0;
 	if (timer_start(timer_socket, timer, 3) < 0) {
 		fprintf(stderr, "timer-test: Failed to start timer 3\n");
+	} else {
+		printf("Started timer 3\n");
 	}
 
 	sleep(5);
 
 	if (timer_cancel(timer_socket, 2) < 0) {
 		fprintf(stderr, "timer-test: Failed to cancel timer 2\n");
+	} else {
+		printf("Canceled timer 2\n");
 	}
 
 	timer->tv_sec = 20;
 	timer->tv_usec = 0;
 	if (timer_start(timer_socket, timer, 4) < 0) {
 		fprintf(stderr, "timer-test: Failed to start timer 4\n");
+	} else {
+		printf("Started timer 4\n");
 	}
 
 	sleep(5);
@@ -59,14 +69,20 @@ int main(int argc, char *argv[]) {
 	timer->tv_usec = 0;
 	if (timer_start(timer_socket, timer, 5) < 0) {
 		fprintf(stderr, "timer-test: Failed to start timer 5\n");
+	} else {
+		printf("Started timer 5\n");
 	}
 
 	if (timer_cancel(timer_socket, 4) < 0) {
 		fprintf(stderr, "timer-test: Failed to cancel timer 4\n");
+	} else {
+		printf("Canceled timer 4\n");
 	}
 
 	if (timer_cancel(timer_socket, 8) < 0) {
 		fprintf(stderr, "timer-test: Failed to cancel timer 8\n");
+	} else {
+		printf("Canceled timer 8\n");
 	}
 
 	return 0;
