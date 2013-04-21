a80ef4b26bc177b530e19e13e26c548b05d9c53b
Dan Ziemba <zman0900@gmail.com>
2013-04-21 10:27:06 -0400

    More troll options

--- start_client_support.sh
+++ start_client_support.sh
@@ -13,13 +13,19 @@ if [ $# -ne 2 ]; then
 fi
 
 # Clean
-./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -t &
+#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -t &
 # Garbling
 #./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -g 25 -t &
 # Reording
-#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -se50 -t &
+#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -se10 -t &
 # Reording and garbling
-#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -se50 -g 25 -t &
+#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -se10 -g 25 -t &
+# Reording and garbling and destroying
+#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -se10 -g 25 -x 25 -t &
+# Reording and garbling and destroying and duplication (troll crashes)
+#./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -se10 -g 25 -x 25 -m 25 -t &
+# Garbling and destroying and duplication (troll crashes)
+./troll -S localhost -b 6664 -C $1 -a $2 6665 -x 0 -g 25 -x 25 -m 25 -t &
 TROLL_PID=$!
 ./tcpd $2 $1
 kill $TROLL_PID
--- start_server_support.sh
+++ start_server_support.sh
@@ -13,9 +13,20 @@ if [ $# -ne 2 ]; then
 fi
 
 # Clean
-./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -t &
+#/troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -t &
 # Garbling
 #./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -g 25 -t &
+# Reording
+#./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -se10 -t &
+# Reording and garbling
+#./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -se10 -g25 -t &
+# Reording and garbling and destroying
+#./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -se10 -g25 -x 25 -t &
+# Reording and garbling and destroying and duplication (troll crashes)
+#./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -se10 -g25 -x 25 -m 25 -t &
+# Garbling and destroying and duplication (troll crashes)
+./troll -S localhost -b 6661 -C $1 -a $(expr $2 + 1) 6662 -x 0 -g25 -x 25 -m 25 -t &
+
 TROLL_PID=$!
 ./tcpd $2
 kill $TROLL_PID
